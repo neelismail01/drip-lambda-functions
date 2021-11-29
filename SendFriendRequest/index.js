29275f438d68caee204e2ebab8051da059adb7aa
Neel Ismail <neelismail01@gmail.com>
2021-11-29 02:05:05 -0500

    modularizing jwt authorization

--- SendFriendRequest/index.js
+++ SendFriendRequest/index.js
@@ -37,37 +37,31 @@ async function connectToDatabase() {
   return db;
 }
 
+const checkUserAuthorization = async (authToken, accessTokenSecret) => {
+  return new Promise((resolve, reject) => {
+      jwt.verify(authToken, accessTokenSecret, (err, userId) => {
+          if (err) {
+              reject("Invalid auth token");
+          }
+          resolve(userId);
+      })
+  })
+}
+
 exports.handler = async (event, context) => {
 
   try {
+
     context.callbackWaitsForEmptyEventLoop = false;
 
     const authHeader = event['params']['header']['Authorization'];
     const authToken = authHeader && authHeader.split(" ")[1];
-    let userId = null;
-
-    if (authToken === null) {
-      return {
-        status: 401,
-        body: "You do not have an authorization token."
-      }
-    }
 
     if (accessTokenSecret === null) {
       accessTokenSecret = await getParams('access-token-secret-jwt');
     }
 
-    jwt.verify(authToken, accessTokenSecret, (err, user) => {
-      if (err) {
-        return {
-          status: 403,
-          body: "You do not have a valid authorization token."
-        }
-      }
-
-      userId = user;
-    })
-
+    const userId = await checkUserAuthorization(authToken, accessTokenSecret);
     const db = await connectToDatabase();
 
     const data = {
