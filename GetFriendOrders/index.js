2c373ffd8f27b92db5e4085b573adc8b33203bf5
Neel Ismail <neelismail01@gmail.com>
2021-11-29 02:03:05 -0500

    modularizing jwt authorization + supporting pagination on backend

--- GetFriendOrders/index.js
+++ GetFriendOrders/index.js
@@ -37,39 +37,35 @@ async function connectToDatabase() {
   return db;
 }
 
+const checkUserAuthorization = async (authToken, accessTokenSecret) => {
+  return new Promise((resolve, reject) => {
+      jwt.verify(authToken, accessTokenSecret, (err, userId) => {
+          if (err) {
+              reject("Invalid auth token");
+          }
+          resolve(userId);
+      })
+  })
+}
+
 exports.handler = async (event, context) => {
 
   try {
 
     context.callbackWaitsForEmptyEventLoop = false;
 
-    const authHeader = event["params"]["header"]["Authorization"];
+    const authHeader = event['params']['header']['Authorization'];
     const authToken = authHeader && authHeader.split(" ")[1];
-    let userId = null;
-
-    if (authToken === null) {
-      return {
-        status: 401,
-        body: "You do not have an authorization token.",
-      };
-    }
 
     if (accessTokenSecret === null) {
-      accessTokenSecret = await getParams("access-token-secret-jwt");
+      accessTokenSecret = await getParams('access-token-secret-jwt');
     }
 
-    jwt.verify(authToken, accessTokenSecret, (err, user) => {
-      if (err) {
-        return {
-          status: 403,
-          body: "You do not have a valid authorization token.",
-        };
-      }
-
-      userId = user;
-    });
-
+    const userId = await checkUserAuthorization(authToken, accessTokenSecret);
     const db = await connectToDatabase();
+    const query = event['params']['querystring']['searchTerm'];
+    const limit = parseInt(event['params']['querystring']['limit']);
+    const page = parseInt(event['params']['querystring']['page']);
 
     const friends = await db.collection('friends').aggregate([
       {
@@ -113,8 +109,37 @@ exports.handler = async (event, context) => {
     const orders = await db.collection('orders').aggregate([
       {
         $match: {
-          user: { $in: friendList },
-        },
+          $and: [
+            {
+              user: { $in: friendList },
+            },
+            {
+              $or: [
+                {
+                  brandName: {
+                    $regex: new RegExp(query, "i"),
+                  },
+                },
+                {
+                  caption: {
+                    $regex: new RegExp(query, "i"),
+                  },
+                },
+                {
+                  tags: {
+                    $regex: new RegExp(query, "i")
+                  }
+                }
+              ],
+            },
+          ]
+        }
+      },
+      {
+        $skip: limit * page
+      },
+      {
+        $limit: limit
       },
       {
         $lookup: {
@@ -125,18 +150,21 @@ exports.handler = async (event, context) => {
         },
       },
       {
-        $sort: {
-          dateOrdered: -1,
-        },
+          $sort: {
+              datePosted: -1
+          }
       },
       {
         $project: {
           user: { $arrayElemAt: ['$user', 0] },
-          orderItems: 1,
-          dateOrdered: 1,
-          likedBy: 1,
-          totalPrice: 1,
-          totalQuantity: 1
+          pictureUrls: 1,
+          tags: 1,
+          brandName: 1,
+          brandLogo: 1,
+          brandWebsite: 1,
+          caption: 1,
+          datePosted: 1,
+          likedBy: 1
         }
       }
     ])
